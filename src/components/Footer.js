0db14a964094df65ce2ea2fdf74479d4b6111b95
Ganeshreact14 <63612111+Ganeshreact14@users.noreply.github.com>
2020-04-14 01:15:45 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const Footer = props => {
+  return (
+    <footer>
+      <p className="footer-links">
+        &copy; copyright
+	  </p>
+    </footer>
+  );
+};
+
+export default Footer;
